706d002fc29e1351a5d109abac099d172e8c60b8
Anna Leoshko <anna.leoshko@gmail.com>
2022-11-27 20:18:36 +0100

    save js file

---
+++ script.js
@@ -0,0 +1,9 @@
+let num1 = prompt('Enter the first number'); 
+num1 = +num1; 
+let num2 = prompt('Enter the second number'); 
+num2 = +num2; 
+ 
+alert(`Множення: ${num1 * num2}`); 
+alert(`Ділення: ${num1 / num2}`); 
+alert(`Додавання: ${num1 + num2}`); 
+alert(`Віднімання: ${num1 - num2}`);
