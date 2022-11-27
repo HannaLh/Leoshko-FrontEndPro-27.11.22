fde52a04c378ca5e615569a51361f159c28ca2d9
Anna Leoshko <anna.leoshko@gmail.com>
2022-11-27 20:43:28 +0100

    update branch

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ num1 = +num1;
 let num2 = prompt('Enter the second number'); 
 num2 = +num2; 
  
-alert(`Множення: ${num1 * num2}`); 
-alert(`Ділення: ${num1 / num2}`); 
-alert(`Додавання: ${num1 + num2}`); 
-alert(`Віднімання: ${num1 - num2}`);
+alert(num1 + " * " + num2 + " = " + `${num1 * num2}`); 
+alert(num1 + " / " + num2 + " = " + `${num1 / num2}`); 
+alert(num1 + " + " + num2 + " = " + `${num1 + num2}`); 
+alert(num1 + " - " + num2 + " = " + `${num1 - num2}`);
