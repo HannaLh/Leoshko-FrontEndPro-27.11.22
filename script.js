289d500d599392d099bb6785c61fcf8d93993375
Anna Leoshko <anna.leoshko@gmail.com>
2022-11-28 10:49:16 +0100

    added +prompt instead of number1 = +number1, changed let to const

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-let number1 = prompt('Enter the first number'); 
-number1 = +number1; 
-let number2 = prompt('Enter the second number'); 
-number2 = +number2; 
-let number3 = prompt('Enter the third number'); 
-number3 = +number3; 
+let number1 = +prompt('Enter the first number'); 
+let number2 = +prompt('Enter the second number'); 
+let number3 = +prompt('Enter the third number'); 
  
 alert((number1 + number2 + number3)/3); 
 
