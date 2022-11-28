e707784c4611045abd7c980f28456afb1aa212a7
Anna Leoshko <anna.leoshko@gmail.com>
2022-11-28 09:20:19 +0100

    refactoring of num1 = +num1 to +prompt, changing let to const

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-let num1 = prompt('Enter the first number');
-num1 = +num1; 
-let num2 = prompt('Enter the second number'); 
-num2 = +num2; 
+let num1 = +prompt('Enter the first number');
+let num2 = +prompt('Enter the second number'); 
  
 alert(num1 + " * " + num2 + " = " + `${num1 * num2}`); 
 alert(num1 + " / " + num2 + " = " + `${num1 / num2}`); 
