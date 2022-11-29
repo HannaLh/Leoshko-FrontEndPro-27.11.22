8e7fd758726ba09cf502d11bbd6f6af19d67ed65
Anna Leoshko <anna.leoshko@gmail.com>
2022-11-29 15:56:05 +0100

    the task from statement if to switch was implemented

--- script.js
+++ script.js
@@ -1,36 +1,18 @@
-let birthYear = prompt('Введіть рік свого народження');
-let currentYear = 2022;
-let city = prompt('Введіть місто, в якому проживаєте');
-let favSport = prompt('Введіть улюблений вид спорту');
+let numOrStr = prompt('input number or string');
+console.log(numOrStr)
 
-switch(birthYear) {
+switch(Boolean(numOrStr) || numOrStr) {     //або switch(!!numOrStr  numOrStr)
     case null:
-        alert("Шкода, ви не вказали рік народження");
+        console.log('ви скасували');
+        break;
+    case '':
+        console.log('Empty String');
+        break;
+    case isNaN(+numOrStr):
+        console.log('number is Ba_NaN');
         break;
     default:
-        alert(`Тобі ${currentYear - birthYear} років`);
-}
-
-
-    if (birthYear === null || city === null && favSport === null) {
-
-        alert("шкода, не ответили");
-}
-else if (city === "Вашингтон" && favSport === "футбол") {
-    alert(`Тобі ${currentYear - birthYear} років` + "\n" + "Ти живеш у столиці США" + "\n" + "Круто! Хочеш стати Ліонель Мессі?");
-}
-else if (city === "Лондон") {
-    alert(`Тобі ${currentYear - birthYear} років` + "\n" + "Ти живеш у столиці Великобританії");
-}
-else if (city === "Київ") {
-    alert(`Тобі ${currentYear - birthYear} років` + "\n" 
-    + "Ти живеш у столиці України");
-}
-else if (city !== "Київ", "Лондон", "Вашингтон") {
-    alert(`Тобі ${currentYear - birthYear} років` + "\n" + "Ти живеш у місті " + city);
-}
-else {
-    alert("Шкода, ти не вказав...");
+        console.log('OK!');
 }
 
 
